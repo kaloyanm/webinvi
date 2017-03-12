db7b28b539156872d3b4f64033617991eb781ee8
kaloyan <kaloyan.mir4ev@gmail.com>
2017-03-13 00:22:54 +0200

    Leave django to server local static files automatically

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,12 +1,10 @@
+
 from django.conf import settings
 from django.conf.urls import url, include
 from django.conf.urls.static import static
-
 from django.contrib import admin
 
-urlpatterns = static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-urlpatterns += [
+urlpatterns = [
     url(r'^invoices/', include('invoices.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^', include('core.urls')),
