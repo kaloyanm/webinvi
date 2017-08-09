b2dc835b287ffbbdd96e69ba4931343b46941704
kaloyan <kaloyan.mir4ev@gmail.com>
2017-08-09 14:54:24 +0300

    add local deploy

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 PROJECT_PATH="/home/ubuntu"
 ${PROJECT_PATH}/virtualenvs/prjenv/bin/pip3.6 install -r ${PROJECT_PATH}/invoiceapp/requirements.txt
+${PROJECT_PATH}/invoiceapp/manage.py migrate
+${PROJECT_PATH}/invoiceapp/manage.py collectstatic --noinput --verbosity=3
