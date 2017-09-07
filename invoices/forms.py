ceccf2eaa503e981f40dc3dc0b07e57b7346aed4
kaloyan <kaloyan.mir4ev@gmail.com>
2017-09-07 15:42:34 +0300

    Add form labels translations

--- invoices/forms.py
+++ invoices/forms.py
@@ -1,8 +1,9 @@
 
 from django import forms
 from django.forms import formset_factory
+from django.utils.translation import ugettext_lazy as _
 
-from core.mixins import InlineFieldsMixin
+from core.mixins import InlineFieldsMixin, TranslateLabelsFormMixin
 from invoices.models import Invoice
 
 
@@ -17,9 +18,30 @@ class InvoiceItemForm(forms.Form):
 InvoiceItemFormSet = formset_factory(InvoiceItemForm)
 
 
-class InvoiceForm(InlineFieldsMixin, forms.ModelForm):
+class InvoiceForm(TranslateLabelsFormMixin, InlineFieldsMixin, forms.ModelForm):
 
     inline_fields = ["__all__"]
+    translate_labels = {
+        "client_name": _("Получател"),
+        "client_city": _("Град"),
+        "client_eik": _("Булстат"),
+        "client_dds": _("Ин по ДДС"),
+        "client_address": _("Адрес"),
+        "client_mol": _("МОЛ"),
+        "number": _("Номер"),
+        "payment_type": _("Начин на плащане"),
+        "payment_iban": _("IBAN"),
+        "payment_swift": _("SWIFT"),
+        "payment_bank": _("Банка"),
+        "tax_base": _("Данъчна основа"),
+        "dds_percent": _("ДДС"),
+        "total": _("Крайна цена"),
+        "accepted_by": _("Приел"),
+        "created_by": _("Съставил"),
+        "verbally": _("Словом"),
+        "taxevent_at": _("Данъчно събитие"),
+        "released_at": _("Дата на издаване"),
+    }
 
     tax_base = forms.DecimalField(disabled=True, required=False)
     total = forms.DecimalField(disabled=True, required=False)
