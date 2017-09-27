bf9df311e8a73580ce9fca50b76b0b0c771f9190
kaloyan <kaloyan.mir4ev@gmail.com>
2017-09-27 21:32:26 +0300

    Cleanup

--- core/assets/webpack-entry.app.js
+++ core/assets/webpack-entry.app.js
@@ -8,8 +8,3 @@ import '../../invoices/assets/js/app.js';
 $('#delete-invoice').on('click', function() {
   $('#delete-invoice-modal').modal({duration: 150}).modal('show');
 });
-
-$('.change-company')
-  .on('click', function() {
-    alert("Company Change");
-  });
