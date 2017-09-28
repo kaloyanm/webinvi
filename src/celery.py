ba4a08f92a9e4393d60a71fa0e821a5a372b80f7
Svetlozar Argirov <zarrro@gmail.com>
2017-09-28 21:23:08 +0300

    Add sentry/raven to django and celery

--- src/celery.py
+++ src/celery.py
@@ -1,6 +1,29 @@
 from __future__ import absolute_import, unicode_literals
 import os
+import logging
 from celery import Celery
+from raven import Client
+from raven.contrib.celery import register_signal, register_logger_signal
+
+
+# Setup raven/sentry
+client = Client(os.environ.get("SENTRY_DSN"))
+
+# register a custom filter to filter out duplicate logs
+register_logger_signal(client)
+
+# The register_logger_signal function can also take an optional argument
+# `loglevel` which is the level used for the handler created.
+# Defaults to `logging.ERROR`
+register_logger_signal(client, loglevel=logging.INFO)
+
+# hook into the Celery error handler
+register_signal(client)
+
+# The register_signal function can also take an optional argument
+# `ignore_expected` which causes exception classes specified in Task.throws
+# to be ignored
+register_signal(client, ignore_expected=True)
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings')
@@ -19,4 +42,5 @@ app.autodiscover_tasks()
 
 @app.task(bind=True)
 def debug_task(self):
+    raise Exception('Hello from celery')
     print('Request: {0!r}'.format(self.request))
